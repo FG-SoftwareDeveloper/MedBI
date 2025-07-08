dfb8ddeb0d563bba9ed13e6ad3a444c8636cfd59
Fg-SoftwareDeveloper <faith.garzasoftwaredeveloper@gmail.com>
2025-07-08 13:53:07 -0500

    sidebar-to-have-a-modern-claymorphic-look-with-shadows-and-soft-borders-so-that-it-matches-the-ui-aesthetic-and-feels-premium--Completed

---
+++ MedBI.Data/Models/NavigationItemRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedBI.Data.Models
+{
+    public class NavigationItemRole
+    {
+        public int Id { get; set; }
+        public int NavigationItemId { get; set; }
+        public string RoleName { get; set; } = string.Empty;
+
+        public NavigationItem? NavigationItem { get; set; }
+    }
+}
